6a7f5462819d438a24eaf26c63711950b3eca1a8
deepasn08 <105222029+deepasn08@users.noreply.github.com>
2022-05-11 22:10:47 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -11,7 +11,7 @@
     }
  alerter_funcptr alerters[] = {emailAlerter, ledAlerter};
 
- void check_and_alert(maxThreshold, alerters, computedStats);
+ void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
 {
         if (computedStats.average > maxThreshold)
         {
