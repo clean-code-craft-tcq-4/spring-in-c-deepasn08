bf528a6c2ea7ec4b487ac8a86bc4d9cc5730781b
deepasn08 <105222029+deepasn08@users.noreply.github.com>
2022-05-11 21:51:57 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -1,1 +1,20 @@
+#include <alerter.h>
 
+  int emailAlerter()
+    {
+        emailAlertCallCount ++;
+    }
+    
+  int ledAlerter()
+    {
+        ledAlertCallCount ++;
+    }
+  
+ int check_and_alert(maxThreshold, alerters, computedStats);
+{
+        if (computedStats.average > maxThreshold)
+        {
+           alerters[0]();
+           alerters[1]();
+        }
+}
