cb0b09bd0afa46eca02f16ae7aa81ff04145da6a
deepasn08 <105222029+deepasn08@users.noreply.github.com>
2022-05-11 21:57:39 +0530

    updated header inclusion in header

--- alerter.h
+++ alerter.h
@@ -1,7 +1,9 @@
-#include <stdio.h>
-#include <stats.h>
+#include "stdio.h"
+#include "stats.h"
 
 extern int emailAlerter();
 extern int int ledAlerter();
 
 extern int check_and_alert(maxThreshold, alerters, computedStats);
+
+
